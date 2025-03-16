8a5ad57461694a31adbb6cf17cf29c2a052ca1fe
Piyush Sharma <autri.piyush@gmail.com>
2025-03-16 19:12:14 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
+const data: any = someFunction();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,5 +12,5 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-const data: any = someFunction()
+
 export default eslintConfig;
