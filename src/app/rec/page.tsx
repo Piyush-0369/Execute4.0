54b52e777481dd59ad7fe7a08e29a9cad4df1838
Piyush Sharma <autri.piyush@gmail.com>
2025-03-16 21:39:35 +0530

    Update page.tsx

--- src/app/rec/page.tsx
+++ src/app/rec/page.tsx
@@ -53,7 +53,7 @@ const RecruiterInterface: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze_resume/", {
+      const response = await fetch("https://ai-vqeh.onrender.com/analyze_resume/", {
         method: "POST",
         body: formData,
       });
